b76047138d4355688021bbaf43e9a24462d5f6bf
Maksim Ivanov <satansdeer@gmail.com>
2020-07-03 18:02:20 +0200

    Add code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+
+const normalizeCardNumber = (value) => {
+  return value.replace(/\s/g, "").match(/.{1,4}/g)?.join(" ").substr(0, 19) || ""
+}
 
 function App() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="cardNumber">Card Number:</label>
+        <input
+          placeholder="0000 0000 0000 0000"
+          type="tel"
+          inputMode="numeric"
+          autoComplete="cc-number"
+          name="cardNumber"
+          id="cardNumber"
+          onChange={(event) => {
+            const {value} = event.target
+            event.target.value = normalizeCardNumber(value)
+          }}
+          ref={register}
+        />
+      </div>
+      <button>Submit</button>
+    </form>
   );
 }
 
